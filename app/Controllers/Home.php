95018182f13f5e6ab622a2782eceda7c8115635f
Mateus Taveira <mateus.staveira@gmail.com>
2023-02-05 19:45:30 -0300

    Home: Contadores para cada tipo de registros.

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,14 +2,40 @@
 
 namespace App\Controllers;
 
+use App\Models\ClienteModel;
+use App\Models\FuncionarioModel;
+use App\Models\ProdutoModel;
 use CodeIgniter\Controller;
 
 class Home extends Controller
 {
+    private $cliModel;
+    private $funcModel;
+    private $prodModel;
+    
+    function __construct()
+    {
+        $this->cliModel = new ClienteModel();
+        $this->funcModel = new FuncionarioModel();
+        $this->prodModel = new ProdutoModel();
+    }
+
     public function index()
     {
+        $data['countCli'] = count(
+            $this->cliModel->findAll()
+        );
+
+        $data['countFunc'] = count(
+            $this->funcModel->findAll()
+        );
+
+        $data['countProd'] = count(
+            $this->prodModel->findAll()
+        );
+
         echo View('templates/header');
-        echo View('home/index');
+        echo View('home/index', $data);
         echo View('templates/footer');
     }
 }
--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -1,11 +1,11 @@
 <?php
-    $session = session();
-    $alert = $session->get('alert');
-    $nomUsu = $session->get('nomUsu');
+$session = session();
+$alert = $session->get('alert');
+$nomUsu = $session->get('nomUsu');
 ?>
 
-<?php if (!empty($alert)): ?>
-    
+<?php if (!empty($alert)) : ?>
+
     <!-- SweetAlert2 -->
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link rel="stylesheet" href="<?= base_url('theme/dist/css/sweetalert2.min.css') ?>">
@@ -31,13 +31,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Página de Exemplo</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Página de Exemplo</a></li>
-                        <li class="breadcrumb-item active">Starter Page</li>
-                    </ol>
+                    <h1 class="m-0">Página Inicial</h1>
                 </div><!-- /.col -->
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
@@ -48,63 +42,37 @@
     <div class="content">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-
-                            <p class="card-text">
-                                Some quick example text to build on the card title and make up the bulk of the card's
-                                content.
-                            </p>
-
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
-                        </div>
-                    </div>
-
-                    <div class="card card-primary card-outline">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-
-                            <p class="card-text">
-                                Some quick example text to build on the card title and make up the bulk of the card's
-                                content.
-                            </p>
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
-                        </div>
-                    </div><!-- /.card -->
-                </div>
-                <!-- /.col-md-6 -->
-                <div class="col-lg-6">
-                    <div class="card">
-                        <div class="card-header">
-                            <h5 class="m-0">Featured</h5>
-                        </div>
-                        <div class="card-body">
-                            <h6 class="card-title">Special title treatment</h6>
-
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-
-                    <div class="card card-primary card-outline">
-                        <div class="card-header">
-                            <h5 class="m-0">Featured</h5>
-                        </div>
-                        <div class="card-body">
-                            <h6 class="card-title">Special title treatment</h6>
-
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div class="col-md-2">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-info"><i class="fas fa-users"></i></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text">Clientes</span>
+                            <span class="info-box-number"><?= $countCli ?></span>
                         </div>
                     </div>
                 </div>
-                <!-- /.col-md-6 -->
+
+                <div class="col-md-2">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-success"><i class="fas fa-users"></i></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text">Funcionários</span>
+                            <span class="info-box-number"><?= $countFunc ?></span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-2">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-warning"><i class="fas fa-box-open"></i></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text">Produtos</span>
+                            <span class="info-box-number"><?= $countProd ?></span>
+                        </div>
+                    </div>
+                </div>
+
             </div>
-            <!-- /.row -->
         </div><!-- /.container-fluid -->
     </div>
     <!-- /.content -->
